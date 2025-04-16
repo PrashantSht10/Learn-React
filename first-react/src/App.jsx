74f24fd0dbc464c8a11dd7db31f6953126101631
Prashant Shrestha <prashantsht700@gmail.com>
2025-04-16 16:05:19 +0545

    First React commit

---
+++ first-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/MainContent"
+import Navbar from "./components/Navbar"
+
+export default function App() {
+    return (
+        <>
+            <Navbar />
+            <Main />
+        </>
+    )
+}
+
+
---
+++ first-react/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="/src/Images/react-logo.png" alt="React logo" width="40px" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
